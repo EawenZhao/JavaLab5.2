3ecd9ee8b25a9ca40cb6bbd57326985f7b93a760
parala <zyh1999zyh@yeah.net>
2022-11-10 21:04:26 +0800

    fix 1

--- src/Iterator.java
+++ src/Iterator.java
@@ -13,11 +13,11 @@ public class Iterator<T> implements Iterable<T> {
     @Override
     public Object next() {
         currPosition++;
-        return list.get(currPosition - 1);
+        return list.get(currPosition - 1); //return the current value
     }
 
     @Override
     public boolean hasNext() {
-        return currPosition <= list.size() - 1;
+        return currPosition <= list.size() - 1; //list.size() returns the actual size of the list, so needs to minus 1
     }
 }
